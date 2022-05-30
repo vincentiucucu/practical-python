e1fd4ec7a4814a3e7d3465facaa57efa39efd4f5
Vincentiu Ciuraru-Cucu <vincentiuciurarucucu@Vincentius-MacBook-Air.local>
2022-05-30 18:03:51 +0100

    Exercise 1.6 bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+max_bounce_num = 10
+current_height = 100 #metres
+bounce = 1
+
+while bounce <= max_bounce_num:
+    print(bounce, end=' ')
+    print(round(current_height * 3/5, 4))
+    bounce += 1
+    current_height *= 3/5
