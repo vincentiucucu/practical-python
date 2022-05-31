ece768d6625e2fa6914c4cc4abab030fb60e9a9d
vincentiucucu <vincentiu.cucu@gmail.com>
2022-05-31 16:21:44 +0100

    Exercise 1.30 modified pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,23 @@
 # pcost.py
 #
-# Exercise 1.27
-with open('Work/Data/portfolio.csv', 'rt') as portfolio:
+# Exercise 1.30
+'''with open('Work/Data/portfolio.csv', 'rt') as portfolio:
     total_cost = 0
     next(portfolio)
     for line in portfolio:
         entry = line.split(',')
         total_cost += int(entry[1]) * float(entry[2])
-    print(f'Total cost {total_cost:.2f}')
+    print(f'Total cost {total_cost:.2f}')'''
+
+def portfolio_cost(filename):
+    'Calculates the total cost of a portfolio'
+    with open(filename, 'rt') as portfolio:
+        total_cost = 0
+        next(portfolio)
+        for line in portfolio:
+            entry = line.split(',')
+            total_cost += int(entry[1]) * float(entry[2])
+        return total_cost
+
+cost = portfolio_cost('Work/Data/portfolio.csv')
+print(f'Total cost: {cost:.2f}')
