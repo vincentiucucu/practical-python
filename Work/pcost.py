2ee9e56d463601c4b2e759027f4329ea87d2b3b0
vincentiucucu <vincentiu.cucu@gmail.com>
2022-05-31 16:31:14 +0100

    Exercise 1.32 using csv module pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,6 @@
 # pcost.py
 #
-# Exercise 1.30
+# Exercise 1.32
 '''with open('Work/Data/portfolio.csv', 'rt') as portfolio:
     total_cost = 0
     next(portfolio)
@@ -8,19 +8,17 @@
         entry = line.split(',')
         total_cost += int(entry[1]) * float(entry[2])
     print(f'Total cost {total_cost:.2f}')'''
-
-from tkinter import E
-
+import csv
 
 def portfolio_cost(filename):
     'Calculates the total cost of a portfolio'
     with open(filename, 'rt') as portfolio:
         total_cost = 0
-        next(portfolio)
-        for line in portfolio:
-            entry = line.split(',')
+        rows = csv.reader(portfolio)
+        next(rows)
+        for row in rows:
             try:
-                total_cost += int(entry[1]) * float(entry[2])
+                total_cost += int(row[1]) * float(row[2])
             except ValueError as e:
                 print(f'Warning: bad line; {e}')
         return total_cost
