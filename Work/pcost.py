513682d48b9640fee6a20470397c2fb22a568dd0
vincentiucucu <vincentiu.cucu@gmail.com>
2022-05-31 16:25:54 +0100

    Exercise 1.31 error handling pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -9,6 +9,9 @@
         total_cost += int(entry[1]) * float(entry[2])
     print(f'Total cost {total_cost:.2f}')'''
 
+from tkinter import E
+
+
 def portfolio_cost(filename):
     'Calculates the total cost of a portfolio'
     with open(filename, 'rt') as portfolio:
@@ -16,8 +19,11 @@ def portfolio_cost(filename):
         next(portfolio)
         for line in portfolio:
             entry = line.split(',')
-            total_cost += int(entry[1]) * float(entry[2])
+            try:
+                total_cost += int(entry[1]) * float(entry[2])
+            except ValueError as e:
+                print(f'Warning: bad line; {e}')
         return total_cost
 
-cost = portfolio_cost('Work/Data/portfolio.csv')
+cost = portfolio_cost('Work/Data/missing.csv')
 print(f'Total cost: {cost:.2f}')
