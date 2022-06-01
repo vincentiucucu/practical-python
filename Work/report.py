f6d80bd0a09d43b7452f9feccc874e9ef29d80b2
vincentiucucu <vincentiu.cucu@gmail.com>
2022-06-01 14:07:16 +0100

    Exercises 2.9 to 2.12 report.py

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercises 2.4, 2.5, 2.6
+# Exercises 2.9, 2.10, 2.11, 2.12
 import csv
 from multiprocessing.dummy import current_process
 from pprint import pprint
@@ -24,6 +24,28 @@ def read_prices(filename):
                 prices[row[0]] = float(row[1])
     return prices
 
+def make_report(portfolio, prices):
+    report = []
+    for i in portfolio:
+        change = prices[i['name']] - i['price']
+        current_price = '$' + str(prices[i['name']])
+        record = (i['name'], i['shares'], current_price, change)
+        report.append(record)
+    return report
+
+# get table info
+portfolio = read_portfolio('Work/Data/portfolio.csv')
+current_prices = read_prices('Work/Data/prices.csv')
+report = make_report(portfolio, current_prices)
+# print header and separating line
+headers = ('Name', 'Shares', 'Price', 'Change')
+print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+print('-' * 10, '-' * 10, '-' * 10, '-' * 10)
+# print values
+for name, shares, price, change in report:
+    print(f'{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}')
+
+'''
 initial_value = 0
 gainloss = 0
 portfolio = read_portfolio('Work/Data/portfolio.csv')
@@ -35,4 +57,6 @@ current_value = initial_value + gainloss
 print(f'Initial value of portfolio: {initial_value:.2f}')
 print(f'Current value of portfolio: {current_value:.2f}')
 print(f'Gain/Loss: {gainloss:.2f}')
+'''
+
 
